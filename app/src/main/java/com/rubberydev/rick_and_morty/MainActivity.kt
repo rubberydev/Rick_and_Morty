e86d25e07fcf6f314cce5e207c7eb688a67465ac
jherrera <jherrera@ingeneo.local>
2022-12-30 16:18:43 -0500

    built and config initial structure

---
+++ app/src/main/java/com/rubberydev/rick_and_morty/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rubberydev.rick_and_morty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
