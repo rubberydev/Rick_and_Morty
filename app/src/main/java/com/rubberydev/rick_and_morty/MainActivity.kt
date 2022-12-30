f2b9201ef2a154fa164e27eb3792e1e2b213d7b2
jherrera <jherrera@ingeneo.local>
2022-12-30 16:49:06 -0500

    added buildFeatures to use viewBinding and binding view with activity

--- app/src/main/java/com/rubberydev/rick_and_morty/MainActivity.kt
+++ app/src/main/java/com/rubberydev/rick_and_morty/MainActivity.kt
@@ -2,10 +2,13 @@ package com.rubberydev.rick_and_morty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rubberydev.rick_and_morty.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
