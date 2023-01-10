2f2190fa41d5c2481b695c2555501eace9cea15d
jherrera <jherrera@ingeneo.local>
2023-01-09 23:04:19 -0500

    network validation

--- app/src/main/java/com/rubberydev/rick_and_morty/ui/main/MainActivity.kt
+++ app/src/main/java/com/rubberydev/rick_and_morty/ui/main/MainActivity.kt
@@ -3,12 +3,14 @@ package com.rubberydev.rick_and_morty.ui.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import com.rubberydev.rick_and_morty.databinding.ActivityMainBinding
 import com.rubberydev.rick_and_morty.model.CharacterDbClient
 import kotlinx.coroutines.launch
 import com.rubberydev.rick_and_morty.model.Character
 import com.rubberydev.rick_and_morty.ui.detail.DetailActivity
+import com.rubberydev.rick_and_morty.utilities.Network.Companion.isConnected
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,8 +20,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val charactersAdapter = CharactersAdapter(emptyList()){ navigateTo(it) }
-
         binding.recycler.adapter = charactersAdapter
+
+        if(!isConnected(this)){
+            Toast.makeText(this@MainActivity,"you must enable your internet settings", Toast.LENGTH_SHORT).show()
+            return
+        }
+        
         lifecycleScope.launch {
             val charactersFromApi = CharacterDbClient.service.getCharacters()
             charactersAdapter.characters = charactersFromApi.results
---
+++ app/src/main/java/com/rubberydev/rick_and_morty/utilities/Network.kt
@@ -0,0 +1,32 @@
+package com.rubberydev.rick_and_morty.utilities
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.util.Log
+
+class Network {
+    companion object{
+        fun isConnected(context: Context): Boolean {
+            val connectivityManager =
+                context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            if (connectivityManager != null) {
+                val capabilities =
+                    connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+                if (capabilities != null) {
+                    if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                        Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                        return true
+                    } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                        Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                        return true
+                    } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                        Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                        return true
+                    }
+                }
+            }
+            return false
+        }
+    }
+}
